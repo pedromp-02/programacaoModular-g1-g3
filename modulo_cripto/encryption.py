e9659f449f00535dff2e1e9bf14f9770dc8430ba
Eric Leao <86169515+Fuginko13@users.noreply.github.com>
2022-05-19 21:11:51 -0300

    Código Refatorado
    refatoramos o codigo para ficar mais legivel

--- modulo_cripto/encryption.py
+++ modulo_cripto/encryption.py
@@ -5,27 +5,32 @@ def hash(hasher, password, salt):
   hasher.update(to_be_hashed)
   return salt + hasher.hexdigest()  
 
+def cria_hasher(cryptography):
+    if cryptography.upper() == 'SHA256':
+        return hashlib.sha256()
+    elif cryptography.upper() == 'SHA512':
+        return hashlib.sha512()
+    elif cryptography.upper() == 'SHA1':
+        return hashlib.sha1()
+    elif cryptography.upper() == 'MD5':
+        return hashlib.md5()
+    elif cryptography.upper() == 'SHA384':
+        return hashlib.sha384()
+    elif cryptography.upper() == 'SHA224':
+        return hashlib.sha224()
+    elif cryptography.upper() == 'BLAKE2B':
+        return hashlib.blake2b()
+    elif cryptography.upper() == 'BLAKE2S':
+        return hashlib.blake2s()
+    else:
+        return - 1
+
 
 def generate_hashed_password(cryptography, password, salt, iterations, dklen):
     derivatekey = ''
-    if cryptography.upper() == 'SHA256':
-        hasher = hashlib.sha256()
-    elif cryptography.upper() == 'SHA512':
-        hasher = hashlib.sha512()
-    elif cryptography.upper() == 'SHA1':
-        hasher = hashlib.sha1()
-    elif cryptography.upper() == 'MD5':
-        hasher = hashlib.md5()
-    elif cryptography.upper() == 'SHA384':
-        hasher = hashlib.sha384()
-    elif cryptography.upper() == 'SHA224':
-        hasher = hashlib.sha224()
-    elif cryptography.upper() == 'BLAKE2B':
-        hasher = hashlib.blake2b()
-    elif cryptography.upper() == 'BLAKE2S':
-        hasher = hashlib.blake2s()
-    else:
-        return - 1
+    hasher = cria_hasher(cryptography)
+    if hasher == -1:
+      return -1
     for c in range (dklen):
         for k in range (iterations):
             password = hash(hasher, password, salt)
