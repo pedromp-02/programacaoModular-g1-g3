0474dd1905707432e16b89496a786f206ed760ce
Lucas Deano <71799788+deanolucas@users.noreply.github.com>
2022-06-01 15:58:40 -0300

    Inclusão da política de CORS

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from modulo_login.loginClass import loginClass
 from modulo_projetos.projetosClass import projetosClass
 from modulo_funcionarios.funcionariosClass import funcionariosClass
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api = Api(app)
 
 # Adicionando as controladores a API
@@ -13,5 +15,12 @@ api.add_resource(loginClass, '/login')
 api.add_resource(projetosClass, '/projetos/<id>')
 api.add_resource(funcionariosClass, '/funcionarios/<id>')
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=80)
